c2c67b419daba1bf582a5c92828ac6110dd253f1
QURIAN K <thequrian@gmail.com>
2018-04-10 20:27:16 +0900

    design chore
    css

--- src/components/Index/IndexDescript.tsx
+++ src/components/Index/IndexDescript.tsx
@@ -9,12 +9,14 @@ export default class IndexDescript extends React.PureComponent<{}, {}> {
                 <div className="row">
                     <div className="col-md-10 col-sm-10 text-center center-col">
                         <h2>대학생 청년 IT 네트워크</h2>
-                        <p className="text-med width-90 center-col margin-three"> 
-                            IT 동아리들을 연결하는 네트워크를 구축합니다. 활동지원을 통해 구성원들이 
-                            개인의 역량을 강화하고 상호간의 우의증진과 친목도모를 이루며, 
-                            IT 생태계에 기여 할 수 있는 인재가 되는 기반을 마련하고자 합니다.<br/><br/>
-                            연합단체와 연합행사를 운영하면서 내적으로는 단체운영 ‧ 정보관리, 
-                            외적으로는 다른 단체와의 네트워킹 ‧ 기업연계 ‧ 후원유치의 어려움을 경험했습니다. 
+                        <p className="text-med width-80 center-col margin-three"> 
+                            단체와 기업을 연결하는 중심이 되고, IT 네트워크를 구축하는 것을 목표로 합니다.
+                            나아가 활동지원을 통해 구성원들이 개인의 역량을 강화하고 상호간의 우의증진과 친목도모를 이루며, 
+                            IT 생태계에 기여 할 수 있는 인재가 되는 기반을 마련하고자 합니다.
+                        </p>
+                        <p className="text-med width-90 center-col margin-three">
+                            연합단체와 연합행사를 운영하면서 내적으로는 단체운영 ‧ 정보관리, 외적으로는 
+                            다른 단체와의 네트워킹 ‧ 기업연계 ‧ 후원유치의 어려움을 경험했습니다. 
                             유관 단체들은 에너지 소모 없이 성장에 집중할 수 있도록 돕기 위한 다양한 방법을 모색하고 추진합니다. 
                             이를 통해 각 단체는 자생 할 수 있는 역량을 가지고 가치를 높이며, 
                             나아가 상호 시너지를 통해 더욱 많은 활동을 할 수 있는 기반을 마련하고자 합니다.
@@ -24,13 +26,13 @@ export default class IndexDescript extends React.PureComponent<{}, {}> {
             </div>
             <div className="container-fluid margin-five no-margin-bottom">
                 <div className="row">
-                    <div className="col-md-12 col-sm-12 col-xs-12 bg-main-color padding-three text-center ">
+                    <div className="col-md-12 col-sm-12 col-xs-12 bg-main-color padding-two text-center ">
                         <span className="text-small text-uppercase font-weight-500 white-text letter-spacing-2">
-                            Web Design &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                            Graphics &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                            Magento &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                            WordPress &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                            Applications
+                            Developer &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                            Designer &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                            Planner &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                            Project Manager &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                            Creator
                         </span>
                     </div>
                 </div>
